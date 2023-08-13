2031a4dfd5f6e36a5cb6338b0411929524356b4d
Nolan Jannotta <anjannotta@verizon.net>
2023-08-13 10:03:47 -0700

    smart contract brainstorming

---
+++ src/BoomBox.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.13;
+
+
+contract BoomBox {
+
+    //struct to store all parameters for a speaker
+    // packedEffectsData could be a titely packed uint that holds 
+    // multiple uint8 values. i.e. which effects are installed, 
+    // can only handle a certain amount though.
+    // maybe switch to array?
+
+    // example, some screenTypes shouldnt work for some bodyTypes. Some backgrounds shouldn't
+    // work with some screenTypes etc.
+
+    // idea:
+    // bodyType could come with default values for each one
+    // for example, 
+    // bodyType = 1 (default)
+    // all others are default set to zero
+    // renders default image for body type #1
+    // if you purchase and install screenType #1 for bodyType #1,
+    // it adds the correct version of screenType for the current bodyType.
+
+    // if bodyType = 2 
+    // the rest are set to zero,
+    // renders default image for bodyType 2
+    // if new screenType is added,
+    // renders correct screenType for bodyType2
+
+
+
+    struct Speaker {
+        uint8 bodyType;
+        uint8 screenType;
+        uint8 background;
+        uint8 buttons;
+        uint8 effectsStyle;
+
+        uint packedEffectsData; 
+
+
+    }
+
+
+
+    function install(address song, uint songId, uint speakerId) public {
+        //do stuff
+    }
+
+    function unInstall(address song, uint songId, uint speakerId) public {
+        //do stuff
+    }
+
+
+}
